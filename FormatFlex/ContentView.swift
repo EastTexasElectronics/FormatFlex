7258ba4551a65c86c03f62b52a460140aa438aa4
Robert M Havelaar <Contact@EastTexaselectronics.com>
2024-08-10 10:46:37 -0500

    Initial Commit

---
+++ FormatFlex/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  FormatFlex
+//
+//  Created by Robert Havelaar on 8/10/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ FormatFlex/FormatFlexApp.swift
@@ -0,0 +1,17 @@
+//
+//  FormatFlexApp.swift
+//  FormatFlex
+//
+//  Created by Robert Havelaar on 8/10/24.
+//
+
+import SwiftUI
+
+@main
+struct FormatFlexApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
